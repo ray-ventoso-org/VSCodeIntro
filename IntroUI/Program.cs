4f8c0661656a8994fc44b9a8b890c17c1b039e9a
Ray Ventoso <ray.ventoso@arcticwolf.com>
2021-06-16 12:38:38 -0400

    initial codebase

---
+++ IntroUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new PersonModel() {
+                FirstName = "Tim", 
+                LastName = "Corey"
+            };
+
+            System.Console.WriteLine($"{p.FirstName} {p.LastName} is my name");
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine();
+        }
+    }
+}
