cd917b63373ae957caef33d00ecb36a29786272b
Ray Ventoso <ray.ventoso@arcticwolf.com>
2021-06-16 22:14:10 -0400

    adding a unit test

---
+++ IntroLibrary.Tests/IntroLibraryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace IntroLibrary.Tests
+{
+    public class IntroLibraryTests
+    {
+        [Fact]
+        public void PersonModel_GetFullName_ExpectedResults()
+        {
+
+            var person = new PersonModel();
+            person.FirstName = "Ray";
+            person.LastName = "Ventoso";
+
+            //Act
+            var sut = person.BuildFullName();
+            
+            //Assert
+            Assert.Equal("Ray Ventoso", sut);
+        }
+    }
+}
--- IntroLibrary/PersonModel.cs
+++ IntroLibrary/PersonModel.cs
@@ -6,8 +6,9 @@ namespace IntroLibrary
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Age { get; set; }    
+        public int Age { get; set; }
 
+        public string BuildFullName() => $"{FirstName} {LastName}";
     }
 
 }
